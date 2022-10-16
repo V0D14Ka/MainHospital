406e4401a6d93c7442907f3e928fc962775ce7e5
V0D14ka <minecraft35510880@gmail.com>
2022-10-16 15:07:12 +0300

    Deleted comments

--- Test/UserTests.cs
+++ Test/UserTests.cs
@@ -10,9 +10,6 @@ namespace Test
 
         public UserTests()
         {
-            // Используем библиотеку Moq, чтобы подготавливать тестовые данные
-            // Мы отдаем реализацию интерфейса, но сервису без разницы, что там, ему важно, что удовлетворяется интерфейсу
-            // Таким образом мы подкидываем нужные данные для тестовых сценариев, другими словами, "мокаем" (mock) репозиторий 
             _userRepositoryMock = new Mock<IUserRepository>();
             _userService = new UserService(_userRepositoryMock.Object);
         }
@@ -22,15 +19,13 @@ namespace Test
         {
             var res = _userService.GetUserByLogin(string.Empty);
 
-            Assert.True(res.IsFailure); // Ошибка пустой строки
+            Assert.True(res.IsFailure); 
             Assert.Equal("Invalid login", res.Error);
         }
 
         [Fact]
         public void UserNotFound_ShouldFail()
         {
-            // It.IsAny означает, что мы подготавливаем то, что должен вернуть метод вне зависимости от того, какой логин мы указали
-            // То есть в данном случае, можно скормить любой string, так как мы тестируем сценарий ненахода, нам в любом случае надо вернуть нуль
             _userRepositoryMock.Setup(repository => repository.GetUserByLogin(It.IsAny<string>()))
                 .Returns(() => null);
 
@@ -46,7 +41,7 @@ namespace Test
             User user = new User(1, "001", "Ivan", Role.Administrator, "Admin", "123");
             var res = _userService.Register(user);
 
-            Assert.True(res.IsFailure); // Вернет ошибку т.к интерфейс не реализован
+            Assert.True(res.IsFailure); 
             Assert.Equal("Unable to create user", res.Error); 
         }
         [Fact]
@@ -54,7 +49,7 @@ namespace Test
         {
             var res = _userService.IsUserExists(string.Empty);
 
-            Assert.True(res.IsFailure); // Вернет ошибку пустой строки
+            Assert.True(res.IsFailure); 
             Assert.Equal("Invalid login", res.Error); 
         }
     }
