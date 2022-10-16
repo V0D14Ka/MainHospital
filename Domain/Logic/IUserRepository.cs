118dda9d29db960444479feacd8a9526a5c4050d
V0D14ka <minecraft35510880@gmail.com>
2022-10-16 11:20:13 +0300

    Added logic, usecase for User

---
+++ Domain/Logic/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Logic
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        bool IsUserExists(string login);
+        User? GetUserByLogin(string login);
+        bool CreateUser(User user);
+    }
+}
